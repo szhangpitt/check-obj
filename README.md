e6daee6b8cd443585ba56c3452e34f8daa3d7028
Shaopeng Zhang <szhangpitt@gmail.com>
2016-10-16 13:30:51 -0400

    doc

---
+++ README.md
@@ -0,0 +1,74 @@
+# TOC
+   - [path defs with {path, fn}](#path-defs-with-path-fn)
+<a name=""></a>
+ 
+<a name="path-defs-with-path-fn"></a>
+# path defs with {path, fn}
+checks.
+
+```js
+const pathDefs = require('../index');
+
+const data = {
+    type: 'person',
+    // name 'required string',
+    profile: {
+        firstName: 'Shaopeng',
+        lastName: 'Zhang',
+        age: '30', // wrong type
+        male: true,
+        female: true, // what???
+        hobby: [ // only validate when `age` is $valid
+            'tv',
+            'photography',
+            'sesame street',
+        ]
+    },
+    address: {
+        street: 'Sesame st',
+        city: 'Princeton',
+        state: 'NY',
+        country: 'US',
+    }
+};
+
+const defs = [{
+    path: 'type',
+    fn: (val) => val === 'person' || val === 'stuff'
+}, {
+    path: 'profile.firstName',
+    fn: (val) => val && typeof val === 'string'
+}, {
+    path: 'profile.age',
+    fn: (val) => typeof val === 'number' && val >= 0
+}, {
+    path: 'profile._gener',
+    fn: (val, body) =>
+        typeof body.profile.male === 'boolean' &&
+        typeof body.profile.female === 'boolean' &&
+        body.profile.male !== body.profile.female
+}, {
+    path: 'profile.hobby',
+    fn: (val) => {return Array.isArray(val) && val.length}
+}, {
+    path: ['address', 'country'],
+    fn: (val) => val === 'US'
+}, {
+    path: ['address', 'state'],
+    fn: (val, body, current) =>
+        current.address.country.$valid ?
+        val === 'NJ' : true
+}];
+
+var checkFn = pathDefs(defs);
+var result = checkFn(data);
+
+expect(result.type.$valid).to.be.true;
+expect(result.profile.firstName.$valid).to.be.true;
+expect(result.profile.age.$valid).to.be.false;
+expect(result.profile._gener.$valid).to.be.false;
+expect(result.profile.hobby.$valid).to.be.true;
+expect(result.address.country.$valid).to.be.true;
+expect(result.address.state.$valid).to.be.false;
+```
+
--- test/path-defs.js
+++ test/path-defs.js
@@ -1,63 +1,69 @@
 'use strict';
 
 const expect = require('chai').expect;
-const pathDefs = require('../index');
 
-describe('pathDefs([{path, fn(val, obj, current)}])', function () {
-    const data = {
-        type: 'person',
-        // name 'required string',
-        profile: {
-            firstName: 'Shaopeng',
-            lastName: 'Zhang',
-            age: '30', // wrong type
-            male: true,
-            female: true, // what???
-            hobby: [ // only validate when `age` is $valid
-                'tv',
-                'photography',
-                'sesame street',
-            ]
-        },
-        address: {
-            street: 'Sesame st',
-            city: 'Princeton',
-            state: 'NY',
-            country: 'US',
-        }
-    }
-
-    const defs = [{
-        path: 'type',
-        fn: (val) => val === 'person' || val === 'stuff'
-    }, {
-        path: 'profile.firstName',
-        fn: (val) => val && typeof val === 'string'
-    }, {
-        path: 'profile.age',
-        fn: (val) => typeof val === 'number' && val >= 0
-    }, {
-        path: 'profile._gener',
-        fn: (val, body) =>
-            typeof body.profile.male === 'boolean' &&
-            typeof body.profile.female === 'boolean' &&
-            body.profile.male !== body.profile.female
-    }, {
-        path: 'profile.hobby',
-        fn: (val) => {return Array.isArray(val) && val.length}
-    }, {
-        path: ['address', 'country'],
-        fn: (val) => val === 'US'
-    }, {
-        path: ['address', 'state'],
-        fn: (val, body, current) =>
-            current.address.country.$valid ?
-            val === 'NJ' : true
-    }];
+describe('path defs with {path, fn}', function () {
 
     it('checks', function () {
+        const pathDefs = require('../index');
+
+
+        const data = {
+            type: 'person',
+            // name 'required string',
+            profile: {
+                firstName: 'Shaopeng',
+                lastName: 'Zhang',
+                age: '30', // wrong type
+                male: true,
+                female: true, // what???
+                hobby: [ // only validate when `age` is $valid
+                    'tv',
+                    'photography',
+                    'sesame street',
+                ]
+            },
+            address: {
+                street: 'Sesame st',
+                city: 'Princeton',
+                state: 'NY',
+                country: 'US',
+            }
+        };
+
+
+        const defs = [{
+            path: 'type',
+            fn: (val) => val === 'person' || val === 'stuff'
+        }, {
+            path: 'profile.firstName',
+            fn: (val) => val && typeof val === 'string'
+        }, {
+            path: 'profile.age',
+            fn: (val) => typeof val === 'number' && val >= 0
+        }, {
+            path: 'profile._gener',
+            fn: (val, body) =>
+                typeof body.profile.male === 'boolean' &&
+                typeof body.profile.female === 'boolean' &&
+                body.profile.male !== body.profile.female
+        }, {
+            path: 'profile.hobby',
+            fn: (val) => {return Array.isArray(val) && val.length}
+        }, {
+            path: ['address', 'country'],
+            fn: (val) => val === 'US'
+        }, {
+            path: ['address', 'state'],
+            fn: (val, body, current) =>
+                current.address.country.$valid ?
+                val === 'NJ' : true
+        }];
+
+
         var checkFn = pathDefs(defs);
         var result = checkFn(data);
+
 
         expect(result.type.$valid).to.be.true;
         expect(result.profile.firstName.$valid).to.be.true;
